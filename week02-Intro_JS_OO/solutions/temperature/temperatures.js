5003753898119eca3b986c07f157132ba7658a0e
Carlos Eduardo da Silva <devops@kaduardo.me>
2021-09-20 15:52:47 +0100

    Removing old files for week 2

--- week02-Intro_JS_OO/solutions/temperature/temperatures.js
+++
@@ -1,6 +0,0 @@
-function fahrenheit2celsius(t) { return (t-32)*(5/9) }
-let kelvin2celsius = (t) => t-273.15
-let celsius2reaumur = (t) => t*(4/5)
-
-let temp = 100
-console.log( celsius2reaumur( kelvin2celsius(temp) ))
