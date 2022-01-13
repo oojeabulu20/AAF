7bee6379645bf63293f6d756c45f215835facf5c
Carlos Eduardo da Silva <devops@kaduardo.me>
2022-01-13 15:45:42 +0000

    bringing rest of user controller

--- week11-Testing/solutions/pets-backend/controllers/user.controller.js
+++ week11-Testing/solutions/pets-backend/controllers/user.controller.js
@@ -49,20 +49,81 @@ exports.findAll = (req, res) => {
 
 // Find a single USer with an id
 exports.findOne = (req, res) => {
- 
+    const id = req.params.id;
+
+    User.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({ message: "Not found User with id: " + id});
+            else
+                res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({message: "Error retrieving User with id: " + id});
+        })
 };
  
 // Update a User by the id in the request
 exports.update = (req, res) => {
- 
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
+
+    const id = req.params.id;
+
+    User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update User with id=${id}. Maybe User was not found!`
+                });
+            } else
+                res.send({ message: "User was updated successfully." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating User with id=" + id
+            });
+        });
 };
  
 // Delete a User with the specified id in the request
 exports.delete = (req, res) => {
- 
+    const id = req.params.id;
+
+    User.findByIdAndRemove(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete User with id=${id}. Maybe User was not found!`
+                });
+            } else {
+                res.send({
+                    message: "User was deleted successfully!"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete User with id=" + id
+            });
+        });
 };
  
 // Delete all Users from the database.
 exports.deleteAll = (req, res) => {
- 
+    User.deleteMany({})
+        .then(data => {
+            res.send({
+                message: `${data.deletedCount} Users were deleted successfully!`
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while removing all users."
+            });
+        });
 };
